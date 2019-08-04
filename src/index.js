92e0a29311b6e28da73ed0e86cf29141f85e4ce5
Ben Chislett <chislett.ben@gmail.com>
2019-08-04 21:58:01 +0900

    Add Pomorello button with popup menu

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
+function Start(t, opts) {
+  console.log("Starting pomodoro!");
+  console.log(t, opts);
+}
+
+function Menu(t, opts) {
+  t.popup({
+    title: "Start a Pomodoro",
+
+    items: [
+      {
+        text: "Plain 25/5",
+        callback: Start,
+      }
+    ]
+  });
+}
 
 window.TrelloPowerUp.initialize({
   "card-buttons": async (t, opts) => {
     return [
       {
-        text: 'Click me!',
-        callback: x => console.log("Clicked with: ", JSON.stringify(x))
+        text: "Click me!",
+        callback: Menu,
       }
     ];
   }
