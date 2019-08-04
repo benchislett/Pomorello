9d8c86d5c0c08f58e7d3a714dfa3739f4afa669c
Ben Chislett <chislett.ben@gmail.com>
2019-08-04 17:10:25 +0900

    Add index.html

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-console.log("Application loaded!");
-
 window.TrelloPowerUp.initialize({
   "card-badges": async function(t, opts) {
-    const card = await t.card("all");
-    return [{text: card.id}];
+    const id = await t.card("id");
+    console.log("Got id: ", id);
+    return [{text: id}];
   }
 });
