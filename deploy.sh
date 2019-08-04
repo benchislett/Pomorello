becb716358453dc3c9a9d747094c4a8aef4bf275
Benjamin Chislett <chislett.ben@gmail.com>
2019-08-05 01:13:42 +0900

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,40 +1,10 @@
-## GET GIT INFO
-
-# Get the previous branch name so we can switch back correctly
-OLD_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-# Set the folder that contains the build files
-DEPLOY_FOLDER=public/
-
-# Set the branch where the build files are will go (must already exist)
-DEPLOY_BRANCH=gh-pages
-
 git config user.name "Benjamin Chislett"
 git config user.email "chislett.ben@gmail.com"
 
-## SEND CHANGES TO REMOTE
+export GIT_DEPLOY_DIR=public/
+export GIT_DEPLOY_BRANCH=gh-pages
+export GIT_DEPLOY_REPO=git@github.com:benchislett/Pomorello.git
 
-# Change the head to the deployment branch
-git symbolic-ref HEAD refs/heads/$DEPLOY_BRANCH
+curl https://raw.githubusercontent.com/X1011/git-directory-deploy/master/deploy.sh > deploy_tmp.sh
 
-# Reset the changes
-git --work-tree $DEPLOY_FOLDER reset --mixed --quiet
-
-# Stage all files in the build folder
-git --work-tree $DEPLOY_FOLDER add --all
-
-# Commit the changes, prompt the user for a commit message
-git --work-tree $DEPLOY_FOLDER commit -m "Add build files"
-
-# Push the changes to the remote
-git --work-tree $DEPLOY_FOLDER push origin $DEPLOY_BRANCH
-
-
-## RESET STATE
-
-# Put the head back to where it was initially
-git symbolic-ref HEAD refs/heads/$OLD_BRANCH
-
-# Clean up the git state a bit
-git reset --mixed
-
+bash deploy_tmp.sh -e -m "Update build files"
