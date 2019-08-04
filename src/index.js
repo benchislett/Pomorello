92eab85d296caa60995763579944fd40c742c0aa
Benjamin Chislett <chislett.ben@gmail.com>
2019-08-05 01:26:39 +0900

    Add basic timer badge

--- src/index.js
+++ src/index.js
@@ -26,20 +26,19 @@ window.TrelloPowerUp.initialize({
     ];
   },
   "card-badges": async (t, opts) => {
-    const is_active = await t.card("POMORELLO_ACTIVE");
-    if (!is_active) {
-      return [];
-      console.log("No Pomorello Active");
-    }
-    console.log("Displaying Pomorello");
     return [
       {
         dynamic: async () => {
+          const is_active = await t.card("POMORELLO_ACTIVE");
+          if (!is_active) {
+            return {};
+          }
           const start_ms = await t.card("POMORELLO_START");
           const age_ms = Date.now() - start_ms;
           const age_str = `${(Math.floor(age_ms / 60000) % 60).toFixed(0)}:${(Math.floor(age_ms / 1000) % 60).toFixed(0)}`;
           return {
-            text: "Pomorello: " + age_str
+            text: "Pomorello: " + age_str,
+            refresh: 30
           }
         }
       }
