f89ea34eadac832aedec79c50c96f320b5ed228d
benchislett <chislett.ben@gmail.com>
2019-08-12 07:51:27 +0000

    fix definitions file

--- src/defs.js
+++ src/defs.js
@@ -1,5 +1,9 @@
+const refresh = 10; // Prod: 30s, Debug: 10s
+const set_length = 1000 * 60 * 1; // Prod: 25m, Debug: 1m
+const break_length = 1000 * 60 * 1; // Prod: 5m, Debug: 30s
+
 export {
-  refresh: 10, // 30 for production, 10 for debugging
-  set_length: 1000*60*1, // (in ms) 25 minutes for production, 1m for debugging
-  break_length: 1000*60*0.5, // (in ms) 5 minutes for production, 30s for debugging
+  refresh,
+  set_length,
+  break_length
 };
