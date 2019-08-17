34023f8d19095b891ca9d6142a77f259294b9c6a
Benjamin Chislett <chislett.ben@gmail.com>
2019-08-18 01:22:54 +0900

    Add logging to notifications

--- src/notifications.js
+++ src/notifications.js
@@ -1,5 +1,8 @@
+import { Logger } from "./logger.js"
+
 let status = null;
 async function init() {
+  Logger.trace("Initializing notifications...");
   if (!("Notification" in window)) {
     status = false;
   } else if (Notification.permission === "granted") {
@@ -8,15 +11,23 @@ async function init() {
     const res = await Notification.requestPermission();
     status = res === "granted";
   }
+  Logger.info(`Notification status: ${status}`);
+  return status;
 }
 
 export async function notify(message) {
+  Logger.trace("Creating notification...");
   if (status === null) {
+    Logger.debug("Notification status is null. Still waiting for permission.");
     await init();
   }
 
   if (status) {
+    Logger.trace("Notifications status positive. Sending notification.");
     return new Notification(message);
   }
 }
+
+// Initialize notification status on module load
+init();
 
