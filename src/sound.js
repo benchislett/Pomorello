d373b4fafd322c7ca6be05c89647b2fedb3219a1
Benjamin Chislett <chislett.ben@gmail.com>
2019-08-17 22:37:34 +0900

    Fix typo in sound.js

--- src/sound.js
+++ src/sound.js
@@ -1,7 +1,7 @@
 class Sound {
   constructor(sound) {
     this.sound = document.createElement("audio");
-    this.sound.src = src;
+    this.sound.src = sound;
     this.sound.setAttribute("preload", "auto");
     this.sound.setAttribute("controls", "none");
     this.sound.style.display = "none";
