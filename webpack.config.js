91647645377116de6e87dc3d112c0da1ba969cdf
Benjamin Chislett <chislett.ben@gmail.com>
2019-09-12 13:41:49 -0400

    Use development mode in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: ['./src/index.js'],
-  mode: 'production',
+  mode: 'development',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
